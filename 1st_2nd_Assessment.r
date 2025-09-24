e59b224d083de5419639f7fc6ca3b2b57eef25cf
gobulihx-ui <gobulihx@gmail.com>
2025-09-24 12:38:22 +0100

    The first time commit

---
+++ 1st_2nd_Assessment.r
@@ -0,0 +1,28 @@
+x_vect <- seq(12,2,-2)
+X <- matrix(x_vect,2,3)
+
+Y <- matrix(seq(1,4),2)
+Z <- matrix(seq(4,10,2),2)
+trans_Y <- t(Y)
+trans_X <- t(X)
+Z%*%Y
+Y%*%X
+solve(Y) %*% Y
+solve(Y) %*% X
+solve(Y,X)
+
+myFirstRFunc <- function(n){
+  stopifnot(is.numeric(n), n%%1==0, n>0) # make sure input is a positive integer number
+  total <- 0 # set initial summation
+  seq <- seq(n)
+  for(i in seq[1:length(seq)-1]){
+    if(i %% 2 == 0){
+      total <- total + i
+    }else if(i %% 7 == 0){
+      total <- total + i
+    }
+  }
+  
+  return(total)
+}
+myFirstRFunc(1000)
